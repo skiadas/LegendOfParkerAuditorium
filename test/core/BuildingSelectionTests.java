71d188442f7ffed4a45aba3ac9c3a79a44361030
LDickman <dickmanl23@hanover.edu>
2020-10-21 16:25:53 -0400

    Add SelectBuildingAction class and UserAction interface ref #3

--- test/core/BuildingSelectionTests.java
+++ test/core/BuildingSelectionTests.java
@@ -1,5 +1,11 @@
 package core;
 
+import org.junit.Test;
+
 public class BuildingSelectionTests {
-    
+
+    @Test
+    public void canCreateSelectBuildingAction() {
+        UserAction user = new SelectBuildingAction(1);
+    }
 }
