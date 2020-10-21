fa08f3514097e6ef382240232bf5eb65c7ae626a
drew <kuerzia23@hanover.edu>
2020-10-21 17:01:02 -0400

    created List availableBuildings   ref #4

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,9 +1,10 @@
 package core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    public static List<Building> availableBuildings;
+    public static List<Building> availableBuildings = new ArrayList<>();
 
     public Game(){
 
