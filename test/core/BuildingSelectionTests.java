20e608a650a4f2535ef320212e7f88692bdd1e26
LDickman <dickmanl23@hanover.edu>
2020-10-21 16:39:20 -0400

    Add Game class ref #3

--- test/core/BuildingSelectionTests.java
+++ test/core/BuildingSelectionTests.java
@@ -1,11 +1,28 @@
 package core;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BuildingSelectionTests {
 
     @Test
+    public void canCreateGame() {
+        Game game = new Game();
+    }
+
+
+
+    @Test
     public void canCreateSelectBuildingAction() {
-        UserAction user = new SelectBuildingAction(1);
+        UserAction playerAction = new SelectBuildingAction(1);
+    }
+
+    @Ignore
+    @Test
+    public void canSelectBuildingNumberOne() {
+        UserAction playerAction = new SelectBuildingAction(1);
+        //assertEquals("Donner", playerAction.getLocation());
     }
 }
