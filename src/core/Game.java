8370c4e02f0c77643a4b8a38a328b7034b22701c
Dawson Skirvin <dskirvinc@gmail.com>
2020-10-22 16:03:28 -0400

    added addBuildings method to Game

--- src/core/Game.java
+++ src/core/Game.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    public static List<Building> availableBuildings = new ArrayList<>();
+    public static List<Building> buildings = new ArrayList<>();
     private final Inventory inventory = new Inventory();
     private final Location location = new Location(true);
     //fix this later
 
     public Game() {
+        buildings = new ArrayList<>();
+    }
+
+    public void addBuildings(Building building){
+        buildings.add(building);
     }
 }
--- test/core/GameTest.java
+++ test/core/GameTest.java
@@ -11,8 +11,17 @@ public class GameTest {
     @Test
     public void canCreateGame(){
         List<Building> buildings = new ArrayList<>();
-        assertEquals(buildings, Game.availableBuildings);
+        assertEquals(buildings, Game.buildings);
     }
 
-
+    @Test
+    public void canAddBuildings() {
+        Game game = new Game();
+        Building building1 = new Building(false);
+        game.addBuildings(building1);
+        assertEquals(building1, Game.buildings.get(0));
+        Building building2 = new Building(false);
+        game.addBuildings(building2);
+        assertEquals(building2, Game.buildings.get(1));
+    }
 }
