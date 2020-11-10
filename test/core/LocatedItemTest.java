fd4dbdab3d3ce43ca7199e1e0005ead5c5919148
JoshElfers <elfersj23@hanover.edu>
2020-11-10 14:56:18 -0500

    Created LocatedItemTest . Ref #9

---
+++ test/core/LocatedItemTest.java
@@ -0,0 +1,15 @@
+package core;
+
+import org.junit.Test;
+
+public class LocatedItemTest {
+    @Test
+    public void canCreateKeyTile() {
+        Game g = new Game();
+        Building building = new Building("Donner");
+        g.addBuilding(building);
+
+
+    }
+
+}
